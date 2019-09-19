0056bf96da5f4bd77a10923d0c8d7e433b120179
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2019-09-19 16:20:12 -0300

    Correções no Projeto

--- src/main/java/br/com/josuecamelo/estudos/EstudosApplication.java
+++ src/main/java/br/com/josuecamelo/estudos/EstudosApplication.java
@@ -2,10 +2,8 @@ package br.com.josuecamelo.estudos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(entityManagerFactoryRef = "dialerEntityManagerFactory", transactionManagerRef = "dialerTransactionManager", basePackages = { "br.com.josuecamelo.com.repositories" })
 public class EstudosApplication {
 
 	public static void main(String[] args) {
